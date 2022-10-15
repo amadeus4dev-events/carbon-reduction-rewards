ee49cd8ddfa87055926fbb1c05f84e9626ccc817
jennzhuge <90708293+jennzhuge@users.noreply.github.com>
2022-10-15 12:15:58 +0200

    hi
    hi

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Carbon reduction rewards</h1>
+          <h1 className="mb-5 text-5xl font-bold">Carbon Reduction Rewards</h1>
           <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
           <button className="btn btn-primary">Get Started</button>
         </div>
