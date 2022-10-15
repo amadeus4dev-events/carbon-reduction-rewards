07b69bbee1139af7ca1b160441c668a52adc0c91
vshulev <velizar.shulev@gmail.com>
2022-10-15 20:47:28 +0200

    Remove unused function

--- src/pages/api/hotels.ts
+++ src/pages/api/hotels.ts
@@ -16,16 +16,6 @@ const amadeus = new Amadeus({
   clientSecret: CLIENT_SECRET,
 });
 
-function makeid(length = 64) {
-  var result = "";
-  var characters = "abcdefghijklmnopqrstuvwxyz0123456789";
-  var charactersLength = characters.length;
-  for (var i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-}
-
 type SearchResult = {
   id: number;
   name: string;
