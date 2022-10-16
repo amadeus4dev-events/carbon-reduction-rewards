7763a79e7a68d4b780abd18a2046e0f0a9b22229
vshulev <velizar.shulev@gmail.com>
2022-10-16 13:57:56 +0200

    Add mock people data

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -1,7 +1,20 @@
+import { NextPage } from "next";
 import { FC, ReactNode } from "react";
 import { TrophyIcon } from "@heroicons/react/24/solid";
+import { peers } from "../lib/mocks";
+import people from "../lib/mocks/people.json";
 
 const Leaderboard: NextPage = () => {
+    const getTrophyColor = (i: number) => {
+      if (i === 0) {
+        return "text-yellow-500";
+      }
+      if (i === 1) {
+        return "text-slate-600";
+      }
+      return "text-yellow-700";
+    };
+
     return (
       <div className="container px-8 py-4">
         <h1 className="mt-6 mb-6 text-3xl font-bold tracking-tight text-gray-900">
@@ -15,35 +28,39 @@ const Leaderboard: NextPage = () => {
                 <th></th>
                 <th>Name</th>
                 <th>Role</th>
-                <th>Favorite Color</th>
+                <th>Emitted CO2 in kg</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>
-                  <TrophyIcon
-                    className="flex-shrink-0 w-6 h-6 text-yellow-500 transition duration-75"
-                    fill="currentColor"
-                  />
-                </td>
-                <td>
-                  <div className="flex items-center space-x-3">
-                    <div className="avatar">
-                      <div className="mask mask-circle w-12 h-12">
-                        <img src="https://placeimg.com/192/192/people" alt="Avatar Tailwind CSS Component" />
+              {people.map((person, i) => (
+                <tr key={person.uid}>
+                  <td>
+                    {i < 3 ? (
+                      <TrophyIcon
+                        className={`flex-shrink-0 w-6 h-6 transition duration-75 ` + getTrophyColor(i)}
+                        fill="currentColor"
+                      />
+                    ) : null}
+                  </td>
+                  <td>
+                    <div className="flex items-center space-x-3">
+                      <div className="avatar">
+                        <div className="mask mask-circle w-12 h-12">
+                          <img src={`https://placeimg.com/192/192/people?k=${person.uid}`} />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold">{person.first_name} {person.last_name}</div>
+                        <div className="text-sm opacity-50">{person.address.country}</div>
                       </div>
                     </div>
-                    <div>
-                      <div className="font-bold">Hart Hagerty</div>
-                      <div className="text-sm opacity-50">United States</div>
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  Desktop Support Technician
-                </td>
-                <td>Purple</td>
-              </tr>
+                  </td>
+                  <td>
+                      {person.employment.title}
+                  </td>
+                  <td>12.0</td>
+                </tr>
+              ))}
             </tbody>
             <tfoot>
               <tr>
